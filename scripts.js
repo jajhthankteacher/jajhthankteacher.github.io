f59b70b057d584fe7a0f94f7edcfffecc7ebc4dc
sunlight-git <jajhthankteacher@gmail.com>
2022-05-19 21:55:21 +0800

    main

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
 const footeryear = document.querySelector('.year').innerHTML=new Date().getFullYear();
+const navlistbtn = document.querySelector('#navlistbtn');
+const navlist = document.querySelector('.navlist');
+const body = document.querySelector('body');
+navlistbtn.onclick=function(){
+    navlist.classList.toggle('active');
+    // body.classList.toggle('active');
+}
