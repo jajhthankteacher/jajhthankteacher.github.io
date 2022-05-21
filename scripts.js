7be7b56907fd834048bda2d96ba9b5e0bfb256e8
sunlight-git <jajhthankteacher@gmail.com>
2022-05-21 10:28:27 +0800

    main

--- scripts.js
+++ scripts.js
@@ -2,10 +2,17 @@ const footeryear = document.querySelector('.year').innerHTML=new Date().getFullY
 const navlistbtn = document.querySelector('#navlistbtn');
 const navlist = document.querySelector('.navlist');
 const body = document.querySelector('body');
-
 navlistbtn.onclick=function(){
     navlist.classList.toggle('active');
-    // body.classList.toggle('active');
-    navlistclose.style.display="block";
+    navlistbtn.classList.toggle('active');
 }
-
+//change navlistbtn content when onclick 
+// function changeText(idElement) {
+//     var navlistbtn = document.getElementById('navlistbtn' + idElement);
+//     if (idElement === 1) {
+//         if (navlistbtn.innerHTML === '&ensp;<i class="bi bi-link-45deg"></i>快速連結&ensp;')navlistbtn.innerHTML = '關閉選單';
+//         else {
+//             navlistbtn.innerHTML = '&ensp;<i class="bi bi-link-45deg"></i>快速連結&ensp;';
+//         }
+//     }
+// }
