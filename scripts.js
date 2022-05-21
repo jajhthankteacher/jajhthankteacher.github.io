f6cb9d700961b9ae5d911992c3bd6238894923ad
sunlight-git <jajhthankteacher@gmail.com>
2022-05-21 16:31:38 +0800

    main

--- scripts.js
+++ scripts.js
@@ -13,7 +13,6 @@ navlistbtn.onclick=function(){
 //         if (navlistbtn.innerHTML === '&ensp;<i class="bi bi-link-45deg"></i>快速連結&ensp;')navlistbtn.innerHTML = '關閉選單';
 //         else {
 //             navlistbtn.innerHTML = '&ensp;<i class="bi bi-link-45deg"></i>快速連結&ensp;';
-//         }
+//         } 
 //     }
 // }
-console.log("")
