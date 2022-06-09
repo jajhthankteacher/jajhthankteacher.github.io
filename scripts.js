43234299e7d2cce27dad9cd3ef32cf962e46cf9a
sunlight-git <100701936+rubychen-blog@users.noreply.github.com>
2022-06-09 23:44:24 +0800

    main

--- scripts.js
+++ scripts.js
@@ -2,18 +2,19 @@ const footeryear = document.querySelector('.year').innerHTML=new Date().getFullY
 const navlistbtn = document.querySelector('#navlistbtn');
 const navlist = document.querySelector('.navlist');
 const body = document.querySelector('body');
+
+// toggle menu on phone
 navlistbtn.onclick=function(){
     navlist.classList.toggle('active');
     navlistbtn.classList.toggle('active');
 }
 
-//change navlistbtn content when onclick 
-// function changeText(idElement) {
-//     var navlistbtn = document.getElementById('navlistbtn' + idElement);
-//     if (idElement === 1) {
-//         if (navlistbtn.innerHTML === '&ensp;<i class="bi bi-link-45deg"></i>快速連結&ensp;')navlistbtn.innerHTML = '關閉選單';
-//         else {
-//             navlistbtn.innerHTML = '&ensp;<i class="bi bi-link-45deg"></i>快速連結&ensp;';
-//         } 
-//     }
-// }
+function changeText(idElement) {
+    var navlistbtn = document.getElementById('navlistbtn' + idElement);
+    if (idElement === 1) {
+        if (navlistbtn.innerHTML === '&ensp;<i class="bi bi-link-45deg"></i>快速連結&ensp;')navlistbtn.innerHTML = '關閉選單';
+        else {
+            navlistbtn.innerHTML = '&ensp;<i class="bi bi-link-45deg"></i>快速連結&ensp;';
+        } 
+    }
+}
